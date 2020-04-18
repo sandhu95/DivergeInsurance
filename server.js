856af0d1b9f9afa4ba997809a9c57f31c5fade34
Manpreet <sandhu.07@gamil.com>
2020-04-18 03:27:47 -0800

    Adding various delete, put and get services

--- server.js
+++ server.js
@@ -3,35 +3,68 @@ const express = require('express');
 const app = express();
 app.set('view engine', 'ejs')
 const MongoClient = require("mongodb").MongoClient;
-
+var objectID = require('mongodb').ObjectID
 
 const bodyParser= require('body-parser')
 const url = "mongodb://localhost:27017/GrowingTogether";
 const err = console.error();
+const methodOverride = require('method-override')
+
 
 
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
 
 
 MongoClient.connect(url, { useUnifiedTopology: true }, (err, client) => {
-    if (err) {
-      return console.log(err);
-    }
-    console.log('Connected to Database')
-      db = client.db("GrowingTogether");
-    // start the express web server listening on 8080
-    app.listen(8081, () => {
-      console.log("listening on 8081");
-    });
+  if (err) {
+    return console.log(err);
+  }
+  console.log('Connected to Database')
+    db = client.db("GrowingTogether");
+  // start the express web server listening on 8080
+  app.listen(8081, () => {
+    console.log("listening on 8081");
   });
+});
 
-
-app.get('/home', (req, res) => {
+app.get('/', (req, res) => {
     res.sendFile(__dirname + '/public/home.html');
   });
 
+  app.post('/addCustomer', (req, res) => {
+    db.collection("customer").insertOne(req.body)
+      .then(result => {
+        console.log(result);
+        res.redirect('/customers')
+      })
+      .catch(error => console.error(error))
+  })
+  app.post('/addmessage', (req, res) => {
+    db.collection("message").insertOne(req.body)
+      .then(result => {
+        console.log(result);
+        res.redirect('/showmessage')
+      })
+      .catch(error => console.error(error))
+  })
 
+  app.get('/showmessage', (req, res) => {
+    db.collection('message').find().toArray()
+      .then(results => {
+        res.render('showmessage.ejs', { message: results })
+      })
+      .catch(/* ... */)
+  })
+
+app.get('/customers', (req, res) => {
+    db.collection('customer').find().toArray()
+      .then(results => {
+        res.render('customers.ejs', { customer: results })
+      })
+      .catch(/* ... */)
+  })
 
   app.get('/staff', (req, res) => {
     db.collection('staff').find().toArray()
@@ -48,11 +81,19 @@ app.get('/home', (req, res) => {
       })
       .catch(/* ... */)
   })
-  
+
   app.get('/departments', (req, res) => {
     db.collection('department').find().toArray()
       .then(results => {
         res.render('department.ejs', { department: results })
+      })
+      .catch(/* ... */)
+  })
+
+  app.get('/address', (req, res) => {
+    db.collection('address').find().toArray()
+      .then(results => {
+        res.render('address.ejs', { address: results })
       })
       .catch(/* ... */)
   })
@@ -64,3 +105,46 @@ app.get('/home', (req, res) => {
       })
       .catch(/* ... */)
   })
+
+  app.get('/edit/:id', (req,res) =>{
+      var id = req.params.id
+      db.collection('customer').find({'_id':objectID(id)}).toArray()
+      .then(results => {
+        res.render('ucustomer.ejs',{customer: results})
+      })
+      .catch(/* ... */)
+})
+
+  app.put('/update/:id',(req,res) =>{
+    var id = req.params.id
+    var myobj = { "_id": objectID(id)};
+    var newvalues = {$set: req.body};
+    db.collection("customer").updateOne(myobj, newvalues,function(err, result) {
+      if (err) throw err;
+      console.log("1 document Updated in customer collection");
+      res.status(200);
+      res.redirect("/customers");
+      });
+  })
+
+app.delete('/delete/:id',(req,res) =>{
+  var id = req.params.id
+  var myobj = { "_id": objectID(id) };
+  db.collection("customer").deleteOne(myobj, function(err, result) {
+    if (err) throw err;
+    console.log("1 document deleted in customer collection");
+    res.status(200);
+    res.redirect("/customers");
+    });
+})
+
+app.delete('/deletemessage/:id',(req,res) =>{
+  var id = req.params.id
+  var myobj = { "_id": objectID(id) };
+  db.collection("message").deleteOne(myobj, function(err, result) {
+    if (err) throw err;
+    console.log("1 document deleted in message collection");
+    res.status(200);
+    res.redirect("/showmessage");
+    });
+})
