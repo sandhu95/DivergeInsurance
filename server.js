745faadf9abd3fb9af703bb998a52230be6415ba
Ramanpreet Kaur <ramanpreetkaor@gmail.com>
2020-04-17 23:25:17 -0400

    created ejs files to get data from staff and policy tables from database

---
+++ server.js
@@ -0,0 +1,51 @@
+console.log('Server-side code running');
+const express = require('express');
+const app = express();
+app.set('view engine', 'ejs')
+const MongoClient = require("mongodb").MongoClient;
+
+
+const bodyParser= require('body-parser')
+const url = "mongodb://localhost:27017/GrowingTogether";
+const err = console.error();
+const methodOverride = require('method-override')
+
+
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }))
+
+
+MongoClient.connect(url, { useUnifiedTopology: true }, (err, client) => {
+    if (err) {
+      return console.log(err);
+    }
+    console.log('Connected to Database')
+      db = client.db("GrowingTogether");
+    // start the express web server listening on 8080
+    app.listen(8081, () => {
+      console.log("listening on 8081");
+    });
+  });
+
+
+app.get('/home', (req, res) => {
+    res.sendFile(__dirname + '/public/home.html');
+  });
+
+
+  app.get('/staff', (req, res) => {
+    db.collection('staff').find().toArray()
+      .then(results => {
+        res.render('staff.ejs', { staff: results })
+      })
+      .catch(/* ... */)
+  })
+
+
+  app.get('/policy', (req, res) => {
+    db.collection('policy').find().toArray()
+      .then(results => {
+        res.render('policy.ejs', { policy: results })
+      })
+      .catch(/* ... */)
+  })
